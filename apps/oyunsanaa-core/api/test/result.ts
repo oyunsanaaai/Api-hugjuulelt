df4562285a7590f674e1d344544696eb72992a95
oyunsanaaai <oyunsanaa.ai@gmail.com>
2025-09-17 22:50:56 +0800

    Create result.ts

---
+++ apps/oyunsanaa-core/api/test/result.ts
@@ -0,0 +1,10 @@
+export const config = { runtime: "edge" };
+
+export default function handler() {
+  return new Response(
+    JSON.stringify({
+      ok: true
+    }),
+    { headers: { "content-type": "application/json; charset=utf-8" } }
+  );
+}
