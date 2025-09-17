8bf23173b1162b62a4e2c11a4ead3a1b917f4a0a
oyunsanaaai <oyunsanaa.ai@gmail.com>
2025-09-17 22:38:24 +0800

    Delete apps/oyunsanaa-core/api/ping.ts

--- apps/oyunsanaa-core/api/ping.ts
+++
@@ -1,8 +0,0 @@
-export const config = { runtime: "edge" };
-
-export default function handler() {
-  // Зөвхөн ил тод string буцаана
-  return new Response("ok", {
-    headers: { "content-type": "text/plain; charset=utf-8" }
-  });
-}
