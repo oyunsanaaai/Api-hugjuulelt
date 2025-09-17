5d22404dfb886cf8b111135f047d23e22f7118b9
oyunsanaaai <oyunsanaa.ai@gmail.com>
2025-09-17 18:28:10 +0800

    Update hello.ts

--- apps/oyunsanaa-core/api/hello.ts
+++ apps/oyunsanaa-core/api/hello.ts
@@ -1,4 +1,4 @@
-export const config = { runtime: "edge" }; // зөв
+export const config = { runtime: "edge" };
 
 export default async function handler() {
   return new Response(JSON.stringify({ ok: true }), {
