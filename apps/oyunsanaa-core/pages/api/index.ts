db22e3929848676aabb3f071e9943cf85f6d4db5
oyunsanaaai <oyunsanaa.ai@gmail.com>
2025-09-19 10:47:46 +0800

    Create index.ts

---
+++ apps/oyunsanaa-core/pages/api/index.ts
@@ -0,0 +1,6 @@
+// apps/oyunsanaa-core/pages/api/index.ts
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ ok: true, name: "oyunsanaa-core API" });
+}
